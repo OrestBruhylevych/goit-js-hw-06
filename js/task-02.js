ff692e9a71fb19ba60a7409c86e83c5a7c3d1c89
Bryhilevych Orest <91126054+OrestBruhylevych@users.noreply.github.com>
2022-04-15 14:33:41 +0300

    add task2

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,27 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const refs = {
+  ingredientsList: document.querySelector('#ingredients'),
+};
+
+const itemsOfIngredients = ingredients.map(option => {
+  const ingredientsItemEl = document.createElement('li');
+  ingredientsItemEl.textContent = option;
+  ingredientsItemEl.classList.add('item');
+
+  return ingredientsItemEl;
+});
+
+refs.ingredientsList.append(...itemsOfIngredients);
+
+
+
+
+
+
+
+
+
+
