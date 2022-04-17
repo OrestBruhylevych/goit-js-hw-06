7690d6d12b3dfc28af8707555b9e1851ca085246
Bryhilevych Orest <91126054+OrestBruhylevych@users.noreply.github.com>
2022-04-17 14:55:52 +0300

    add task9

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,21 @@
+const refs = {
+  body: document.querySelector('body'),
+  nameColor: document.querySelector('.color'),
+  buttonChangeColor: document.querySelector('.change-color')
+};
+
+refs.buttonChangeColor.addEventListener('click', onColorChange)
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+function onColorChange(event) {
+  const color = getRandomHexColor();
+
+  refs.body.style.backgroundColor = color;
+  refs.nameColor.textContent = color;
+
+}
+
+
